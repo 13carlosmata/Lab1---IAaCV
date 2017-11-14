b9668f585b1402e8ec22b9812ab386b68d3e7d54
13carlosmata <cmata@kth.se>
2017-11-14 17:26:44 +0100

    q13

--- Lab1.m
+++ Lab1.m
@@ -82,6 +82,7 @@ title('fft2(F)*fft2(G)')
 %% Scaling
 
 % Question 11
+clc
 close all
 figure
 F = [zeros(60, 128); ones(8, 128); zeros(60, 128)] .* ...
@@ -98,83 +99,44 @@ title('fft2(F)');
 %% Rotation
 
 % Question 12
-figure
-
-
-
-
-
-
-
-
-
-%%
-%{
+clc
 close all
 clear all
-%{
-Fhat = zeros(128,128);
-p = 5; q = 9;
-Fhat(p,q) = 1;
+
+alpha = 30;
+figure;
+F = [zeros(60, 128); ones(8, 128); zeros(60, 128)] .* ...
+    [zeros(128, 48) ones(128, 32) zeros(128, 48)];
+
+angles = [30,45,60,90];
+j = 1;
+jplot = 1;
 figure
-subplot(2,4,1)
-imshow(Fhat)
-%showgrey(Fhat);
-F = ifft2(Fhat);   % inverse discrete FT of Fhat
-%}
-Fabsmax = max(abs(F(:)));
-subplot(2,4,5); showgrey(real(F), 64, -Fabsmax, Fabsmax)
-subplot(2,4,6); showgrey(imag(F), 64, -Fabsmax, Fabsmax)
-subplot(2,4,7); showgrey(abs(F), 64, -Fabsmax, Fabsmax)
-subplot(2,4,8); showgrey(angle(F), 64, -pi, pi)
-
-pz = 128;
-fftwave(5,9,pz);
-fftwave(9,5,pz);
-fftwave(17,9,pz);
-fftwave(17,121,pz);
-fftwave(5,1,pz);
-fftwave(121,1,pz);
-function [params] = fftwave(p,q,sz) 
-Fhat = zeros (sz);
-Fhat(p,q) = 1;
-F = ifft2(Fhat);
-Fabsmax = max(abs(F(:)));
-params = figure;
-subplot(3,2,1); showgrey(Fhat);
-title(sprintf('Fhat: (p, q) = (%d, %d)', p, q))
-
-if (p <= sz/2)
-    pc = p - 1;
-else
-    pc = p - 1 - sz
+for i=angles
+    G_x = rot(F,angles(j));
+    Ghat_x = fft2(G_x);
+    Hhat_x = rot(fftshift(Ghat_x), -angles(j));
+   
+    subplot(4,3,jplot);
+    showgrey(G_x);
+    axis on;
+    title('G');
+    
+    subplot(4,3,jplot+1);
+    showfs(Ghat_x);
+    axis on;
+    title('FFT(G)');
+    
+    subplot(4,3,jplot+2);
+    showgrey(log(1+abs(Hhat_x)));
+    axis on;
+    title(sprintf('Rotated by an angle of: %d', angles(j)));
+    
+    
+    
+    j = j + 1;
+    jplot = jplot+3;
 end
-if (q <= sz/2)
-    qc = q - 1;
-else
-    qc = q - 1 - sz
-end
-
-wavelength = 0.0;
-amplitude = 0.0;
-subplot(3, 2, 2);
-%showgrey(Fhat);
-showgrey(fftshift(Fhat));
-title(sprintf('centered Fhat: (pc, qc) = (%d, %d)', pc, qc))
-subplot(3, 2, 3);
-showgrey(real(F), 64, -Fabsmax, Fabsmax);
-title('real(F)')
-subplot(3, 2, 4);
-showgrey(imag(F), 64, -Fabsmax, Fabsmax);
-title('imag(F)')
-subplot(3, 2, 5);
-showgrey(abs(F), 64, -Fabsmax, Fabsmax);
-title(sprintf('abs(F) (amplitude %f)', amplitude))
-subplot(3, 2, 6);
-showgrey(angle(F), 64, -pi, pi);
-title(sprintf('angle(F) (wavelength %f)', wavelength))
-end
-%}  
 
 
 
